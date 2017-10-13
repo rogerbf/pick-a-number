dfcfed50f99a7266b63175c48dc3490d62dcd1af
rogerbf <roger@bergfalk.nu>
2017-10-13 21:57:00 +0200

    experimentation

--- source/index.js
+++ source/index.js
@@ -1,1 +1,16 @@
-export default `pick-a-number`
+const randomNumber = min => max =>
+  Math.floor(Math.random() * (1 + max - min)) + min
+
+const optionsInput = ({ min, max }) => randomNumber(min)(max)
+
+const random = (state = {}) => ({
+  between: min => random({ ...state, min }),
+  from: min => random({ ...state, min }),
+  and: max => optionsInput({ ...state, max }),
+  to: max => optionsInput({ ...state, max })
+})
+
+export default Object.assign(
+  options => optionsInput(options),
+  random()
+)
